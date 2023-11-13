a71cbbebe923c5e1910f41ed6ff00cdc9c6e1bf0
BryceVerberne <bryceverberne@gmail.com>
2023-11-12 19:44:13 -0700

    ✨ Create new .java class to handle defect log information

---
+++ MainUI/src/effortLoggerV2/DefectLogs.java
@@ -0,0 +1,17 @@
+/**
+ * Title:       Defect Logs Class
+ * Author:      Bryce Verberne
+ * Email:       bverbern@asu.edu
+ * Description: 
+ */
+
+
+
+package effortLoggerV2;
+
+import java.util.ArrayList;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class DefectLogs {
+
+}
